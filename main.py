fbab6397d7b6758b608581b7f9b6439b124f3485
Larry Schirmer <larryschirmer@gmail.com>
2020-06-11 02:09:50 +0000

    view top and bottom rows

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import numpy as np
 import pandas as pd
 
-df = pd.DataFrame({'A': 1.,
-                   'B': pd.Timestamp('20130102'),
-                   'C': pd.Series(1, index=list(range(4)), dtype='float32'),
-                   'D': np.array([3] * 4, dtype='int32'),
-                   'E': pd.Categorical(["test", "train", "test", "train"]),
-                   'F': 'foo'})
+# create a DatetimeIndex
+dates = pd.date_range('20130101', periods=6)
+print('\ndates')
+print(dates)
 
-
+# create a Dataframe
+df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
 print('\ndf')
 print(df)
-print('\ndf.types')
-print(df.dtypes)
+
+top5 = df.head()
+bottom3 = df.tail(3)
+
+print('\ntop 5')
+print(top5)
+
+print('\nbottom 3')
+print(bottom3)
