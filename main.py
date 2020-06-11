edbd45cf1691ebcdade2b10dfa42106754bde1f1
Larry Schirmer <larryschirmer@gmail.com>
2020-06-11 02:03:43 +0000

    create a DataFrame from a dict

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import numpy as np
 import pandas as pd
 
-# create a DatetimeIndex
-dates = pd.date_range('20130101', periods=6)
-print('\ndates')
-print(dates)
+df = pd.DataFrame({'A': 1.,
+                   'B': pd.Timestamp('20130102'),
+                   'C': pd.Series(1, index=list(range(4)), dtype='float32'),
+                   'D': np.array([3] * 4, dtype='int32'),
+                   'E': pd.Categorical(["test", "train", "test", "train"]),
+                   'F': 'foo'})
 
-# create a Dataframe
-df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
+
 print('\ndf')
 print(df)
+print('\ndf.types')
+print(df.dtypes)
