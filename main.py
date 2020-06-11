ed4c2271cfbb79968eb0766de390f684ebd0ae2f
Larry Schirmer <larryschirmer@gmail.com>
2020-06-11 01:54:05 +0000

    create a Dataframe from a NumPy array

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import numpy as np
 import pandas as pd
 
-s = pd.Series([1, 3, 5, np.nan, 6, 8])
+# create a DatetimeIndex
+dates = pd.date_range('20130101', periods=6)
+print('\ndates')
+print(dates)
 
-print(s)
+# create a Dataframe
+df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
+print('\ndf')
+print(df)
