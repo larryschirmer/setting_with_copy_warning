b3647cee1631f90b42a66f388112c9a8850f1131
Larry Schirmer <larryschirmer@gmail.com>
2020-06-20 23:37:42 +0000

    select regions by label

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
 print('\ndf')
 print(df)
 
-# select a single column
-print('\n')
-print(df['A'])
+# select by label
+print('\nOne Index')
+print(df.loc[dates[0]])
 
-# select a slice of rows
-print('\n')
-print(df[0:3])
+# select by label
+print('\nAll Indexes with two columns')
+print(df.loc[:, ['A', 'C']])
+
+# select by label
+print('\nScalar at location')
+print(df.at[dates[0], 'C'])
