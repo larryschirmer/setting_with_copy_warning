9a2fad8f9c22b2b872692e53cd8cfc947c29a8cb
Larry Schirmer <larryschirmer@gmail.com>
2020-06-21 00:39:51 +0000

    reindex dataframe and fill in missing values

--- main.py
+++ main.py
@@ -11,23 +11,10 @@ df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
 print('\ndf')
 print(df)
 
-# assign misaligned indexes to dataframe
-column1 = [1, 2, 3, 4, 5, 6]
-indexes = pd.date_range('20130102', periods=6)
-s1 = pd.Series(column1, indexes)
+# reindex dataframe
+df1 = df.reindex(index=dates[0:4].append(pd.date_range(
+    '20130107', periods=2)), columns=list(df.columns) + ['E'])
+print(df1)
 
-df['E'] = s1
-
-print(df)
-
-# assign value at labels
-df.at[dates[0], 'A'] = 0
-print(df)
-
-# reassign column D to 5
-df.loc[:, 'D'] = np.array([5] * len(df))
-print(df)
-
-# inverse values where a value greater than 0
-df[df > 0] = -df
-print(df)
+# fill in missing values
+print(df1.fillna(value=1))
